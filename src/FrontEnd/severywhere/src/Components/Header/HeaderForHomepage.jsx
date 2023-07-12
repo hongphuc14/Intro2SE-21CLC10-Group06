d9bd8431473dd10c2abd84dda0f9376a14005707
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2023-07-12 21:56:44 +0700

    create header

---
+++ src/FrontEnd/severywhere/src/Components/Header/HeaderForHomepage.jsx
@@ -0,0 +1,11 @@
+import "./HeaderForHomepage.scss";
+
+export default function Header(){
+    return(
+        <div className = "headerForHomepage">
+            <a href = "https://www.facebook.com/" svg="logo_horizon" alt = "logo"> logo</a>
+           <a href = "https://www.google.com/">About & FAQs</a>
+        </div>
+    )
+}
+
