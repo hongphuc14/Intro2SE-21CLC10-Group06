9d51060e53cdba55f8cd03cf8148f0dd68baaa05
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2023-07-29 01:31:33 +0700

    update redux store

--- src/FrontEnd/severywhere/src/redux/configStore.js
+++ src/FrontEnd/severywhere/src/redux/configStore.js
@@ -2,6 +2,9 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools} from "redux-devtools-extension";
 
+const rootReducer = combineReducers({
+    
+})
 
 // cấu hình thunk
 const middleWare = [thunk];
