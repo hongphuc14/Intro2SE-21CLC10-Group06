c52cf2b6da6a6e8cecef6c06c159fe6d59856538
Nguyen Minh Minh <nguyenthiminhminh6a1@gmail.com>
2023-08-08 01:24:32 +0700

    side nav barar

--- src/FrontEnd/severywhere/src/App.js
+++ src/FrontEnd/severywhere/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import Navbar from './Components/Header/HeaderGuest';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
+import EditProfile from './Pages/Profile/EditProfile';
 import {Route, Switch, Router} from 'react-router-dom';
 import { createBrowserHistory } from "history";
 // import Header from './Components/Header/HeaderForHomepage';
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route path = "/signup" exact component={Signup} />
         <Route path = "/login" exact component ={Login} />
+        <Route path = "/editprofile" exact component = {EditProfile} />
       </Switch>
     </div>
   </Router>
---
+++ src/FrontEnd/severywhere/src/Components/SideNavBar/NavBar.jsx
@@ -0,0 +1,59 @@
+import "./NavBar.scss"
+
+const NavBar = () => {
+    return (
+        <nav className="sidebar">
+            <div className="user-profile">
+                <div className="profile-image">
+                    <img src="avatar.png" alt="User Profile"/>
+                </div>
+            </div>
+            <div className="profile-name">
+                Minh Minh
+                <div className="mail">
+                    ntmminh21@clc.fitus.edu.vn
+                </div>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">
+                        <i className="fa-solid fa-pen-to-square"></i>
+                        <span>Edit Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fa-solid fa-lock"></i>
+                        <span>Password</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fa-solid fa-heart"></i>
+                        <span>Wishlist</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fa-solid fa-clock-rotate-left"></i>
+                        <span>Recent</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fa-solid fa-comment-dots"></i>
+                        <span>My Reviews</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fa-solid fa-cart-shopping"></i>
+                        <span>My Orders</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
