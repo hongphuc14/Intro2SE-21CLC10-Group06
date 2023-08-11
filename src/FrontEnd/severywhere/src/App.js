ef8dfba6861e3ea8ccfa0a5751e945da82f39397
Nguyen Minh Minh <nguyenthiminhminh6a1@gmail.com>
2023-08-11 16:46:15 +0700

    finish page resetpassword

--- src/FrontEnd/severywhere/src/App.js
+++ src/FrontEnd/severywhere/src/App.js
@@ -4,6 +4,7 @@ import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
 import EditProfile from './Pages/Profile/EditProfile';
 import ResetPasswordEmail from './Pages/ResetPasswordEmail/ResetPasswordEmail';
+import ResetPassword from './Pages/ResetPassword/ResetPassword';
 import {Route, Switch, Router} from 'react-router-dom';
 import { createBrowserHistory } from "history";
 // import Header from './Components/Header/HeaderForHomepage';
@@ -21,6 +22,7 @@ function App() {
         <Route path = "/login" exact component ={Login} />
         <Route path = "/editprofile" exact component = {EditProfile} />
         <Route path = "/emailResetpw" exact component = {ResetPasswordEmail} />
+        <Route path = "/resetpw" exact component = {ResetPassword} />
       </Switch>
     </div>
   </Router>
