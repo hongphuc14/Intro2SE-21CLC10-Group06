814d23ce22bd45728beb458943d8c2449585b0b7
Minh Minh <nguyenthiminhminh6a1@gmail.com>
2023-08-15 00:34:49 +0700

    update homepage

--- src/FrontEnd/severywhere/src/Pages/HomePage/HomePage.jsx
+++ src/FrontEnd/severywhere/src/Pages/HomePage/HomePage.jsx
@@ -6,14 +6,21 @@ import { SearchBar } from '../../Components/SearchBar/SearchBar'
 import { SearchList } from '../../Components/SearchBar/SearchList'
 import { useState } from 'react'
 
-function HomePage () {
+function HomePage() {
     const [results, setResults] = useState([]);
 
     return (
-        <div className='Searchbar-container'>
-            <SearchBar setResult={setResults}/>
-            {results && results.length > 0 && <SearchList results={results} />}
-        </div>  
+        <div className='HomePage-container'>
+            <HeaderGuest />
+            <div className='HomePage-content'>
+                <h1>SEVERYWHERE</h1>
+                <div className='Searchbar-container'>
+                    <SearchBar setResult={setResults} />
+                    {results && results.length > 0 && <SearchList results={results} />}
+                </div>
+            </div>
+            <Footer />
+        </div>
     );
 }
 
