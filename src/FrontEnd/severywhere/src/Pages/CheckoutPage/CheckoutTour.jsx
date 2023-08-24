da9b2d5840f058334e008fdc4164f764d64e7960
Minh Minh <nguyenthiminhminh6a1@gmail.com>
2023-08-24 19:37:59 +0700

    create card for checkout

---
+++ src/FrontEnd/severywhere/src/Pages/CheckoutPage/CheckoutTour.jsx
@@ -0,0 +1,15 @@
+import './CheckoutTour.scss'
+import React, { useState, useEffect } from 'react'
+import HeaderGuest from '../../Components/Header/HeaderGuest'
+import Footer from '../../Components/Footer/Footer'
+import CheckOutForm from '../../Components/CheckOutForm/CheckOutForm'
+
+const  CheckoutPage= () => {
+    return ( 
+        <div className = "checkout-page">
+            <CheckOutForm/>
+        </div>
+    );
+}
+ 
+export default CheckoutPage;
